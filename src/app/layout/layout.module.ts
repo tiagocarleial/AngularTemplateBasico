4ddeb3744020dec30dc8459ffd4868c4a0d1c695
Tiago Oliveira Carleial <tiago.oliveira@seduc.ce.gov.br>
2022-05-30 15:22:52 -0300

    v1.0

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    RouterModule,
+    MatToolbarModule,
+    MatButtonModule
+  ],
+  exports: [
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent
+  ],
+  declarations: [
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent
+  ]
+})
+export class LayoutModule { }
